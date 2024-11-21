0445f44fd9f7bb5ff3a6cf519fdd5565c6800b90
amir-2005 <amirhh.py@gmail.com>
2024-11-21 23:11:18 +0330

    Define Card Class

--- A4.cpp
+++ A4.cpp
@@ -3,6 +3,32 @@
 
 using namespace std;
 
+class Card
+{
+public:
+    Card(string q, string a, int w = 0)
+    {
+        question = q;
+        answer = a;
+        wrong_answers = w;
+    }
+
+    string getQuestion() { return question; }
+
+    string getAnswer() { return answer; }
+
+    void incWrongAns() { wrong_answers += 1; }
+
+    int getWrongAns() { return wrong_answers; }
+
+    void forgetWrongAns() { wrong_answers = 0; }
+
+private:
+    string question;
+    string answer;
+    int wrong_answers;
+};
+
 
 int main()
 {
